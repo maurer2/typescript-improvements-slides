ecf860d78d325edd9889c520e30968a84af6ca41
Martin <mr.mmaurer+work@gmail.com>
2022-07-26 23:24:44 +0100

    Simplified animal and macbook implementations

--- slides/drafts/structured-typing-vs-nominal-typing/NominalTyping/MacBook.java
+++ slides/drafts/structured-typing-vs-nominal-typing/NominalTyping/MacBook.java
@@ -1,20 +1,5 @@
-public class MacBook implements IMacBookAnimal {
-	private final String name;
-	private final String colour;
-	private final int numberOfFeet;
-	private final boolean hasFans;
-	private final boolean canBeInARoom;
-
-	public MacBook(String name, String colour, int numberOfFeet, boolean hasFans, boolean canBeInARoom) {
-		this.name = name;
-		this.colour = colour;
-		this.numberOfFeet = numberOfFeet;
-		this.hasFans = hasFans;
-		this.canBeInARoom = canBeInARoom;
-	}
-
+public record MacBook(String name, String colour, int numberOfFeet, boolean hasFans, boolean canBeInARoom) implements IMacBookAnimal {
 	public String toString() {
-		return String.format("%1$s, %2$s, %3$s, %4$s, %5$s", this.name, this.colour, this.numberOfFeet, this.hasFans,
-				this.canBeInARoom);
+		return String.format("%1$s, %2$s, %3$s, %4$s, %5$s", this.name, this.colour, this.numberOfFeet, this.hasFans, this.canBeInARoom);
 	}
 }
