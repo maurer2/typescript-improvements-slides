97730f919dcb64b0d9752ed57b148b151463a3ab
Martin <mr.mmaurer+work@gmail.com>
2022-07-30 14:23:45 +0200

    Init example project, added config files and added tailwind

---
+++ code/example-app/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'next',
+  ],
+  env: {
+    browser: true,
+    es2022: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    project: ['tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+};
