35c71f956853fdde44c10cff098a4741179c4bf9
Martin <mr.mmaurer+work@gmail.com>
2022-09-03 23:00:59 +0100

    Improved SortCode type

--- code/examples/number-range/types.ts
+++ code/examples/number-range/types.ts
@@ -1,10 +1,10 @@
 // https://catchts.com/range-numbers
-export type NumberRange<UpperBound extends number, Result extends number[] = []> = (Result['length'] extends UpperBound
-  ? Result
-  : NumberRange<UpperBound, [...Result, Result['length']]>
+export type NumberRange<UpperBound extends number, ArrayWithVariableLength extends number[] = []> = (ArrayWithVariableLength['length'] extends UpperBound
+  ? ArrayWithVariableLength
+  : NumberRange<UpperBound, [...ArrayWithVariableLength, ArrayWithVariableLength['length']]> // recursively call NumberRange until upper bound has been reached
 )
-type SortCodePart = NumberRange<100>[number];
+type SortCodePart = Exclude<NumberRange<100>[number], 0>; // exclude 0 since sort codes, start at 1
 
-type SortCode = [SortCodePart, SortCodePart, SortCodePart]
+type SortCode = [SortCodePart, SortCodePart, SortCodePart];
 
-const sortCode: SortCode = [1, 11, 99]; // todo leading zero
+const sortCode: SortCode = [99, 11, 99];
