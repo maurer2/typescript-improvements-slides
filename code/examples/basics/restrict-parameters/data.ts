318a5fa8e2c8ecca6cd77e58b7d7dbdda5186982
Martin <mr.mmaurer+work@gmail.com>
2022-09-12 00:30:54 +0200

    Improved restrict-parameter example

---
+++ code/examples/basics/restrict-parameters/data.ts
@@ -0,0 +1,14 @@
+import { Person, PersonList } from './types';
+import { faker } from '@faker-js/faker';
+
+export const people: Person[] = Array.from({ length: 5 }, () => ({
+  firstName: faker.name.firstName(),
+  lastName: faker.name.lastName(),
+  isPrimeMinister: false,
+}));
+
+export const peopleList: PersonList = people.map((person) => ({
+  ...person,
+  id: faker.datatype.uuid(),
+  numberOfCats: faker.datatype.number({ min: 0, max: 50, precision: 1 }),
+}));
--- code/examples/basics/restrict-parameters/types.ts
+++ code/examples/basics/restrict-parameters/types.ts
@@ -7,9 +7,6 @@ export type Person = {
 export type PersonList = ReadonlyArray<Pick<Person, 'firstName' | 'lastName'> & {
   id: string,
   numberOfCats: number,
-  firstName: string,
-  lastName: string,
 }>
 
-// export type PersonListFields = keyof PersonList // gets all fields of the array type
 export type PersonListFields = keyof PersonList[number]; // gets all fields of the type of the array
