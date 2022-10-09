c9a18ac5bfc064781dd734476f31d0fd57ebcaab
Martin <mr.mmaurer+work@gmail.com>
2022-10-09 02:27:10 +0100

    Added data-modeling example3

---
+++ code/examples/data-modeling/example3/data.ts
@@ -0,0 +1,28 @@
+import { faker } from '@faker-js/faker';
+import currency from 'currency.js';
+
+import type { LoanStringlyTyped, Loan } from './types';
+
+const loanJSON: LoanStringlyTyped = {
+  id: faker.datatype.uuid(),
+  title: faker.lorem.words(5),
+  tranche: faker.lorem.word(),
+  available_amount: faker.finance.amount(),
+  annualised_return: faker.finance.amount(),
+  term_remaining: faker.date.future(12).toISOString(),
+  ltv: faker.datatype.number({ max: 100, precision: 0.01 }),
+  loan_value: faker.finance.amount(),
+};
+
+const loan: Loan = {
+  id: loanJSON.id,
+  title: loanJSON.title,
+  tranche: loanJSON.tranche,
+  available_amount: currency(loanJSON.available_amount),
+  annualised_return: currency(loanJSON.annualised_return),
+  term_remaining: new Date(loanJSON.term_remaining),
+  ltv: BigInt(Math.trunc(loanJSON.ltv * 100)),
+  loan_value: currency(loanJSON.loan_value),
+};
+
+export const loans: [LoanStringlyTyped, Loan] = [loanJSON, loan];
--- code/examples/data-modeling/example3/types.ts
+++ code/examples/data-modeling/example3/types.ts
@@ -1,6 +1,7 @@
 // adapted from https://github.com/maurer2/loansome
 import currency from 'currency.js';
 
+// JSON
 export type LoanStringlyTyped = {
   id: string;
   title: string;
@@ -8,14 +9,17 @@ export type LoanStringlyTyped = {
   available_amount: string;
   annualised_return: string;
   term_remaining: string;
-  ltv: string;
+  ltv: number;
   loan_value: string;
 };
 
-export type Loan = Omit<LoanStringlyTyped, 'available_amount' | 'annualised_return' | 'loan_value' | 'term_remaining'> & {
-  available_amount: currency,
-  annualised_return: currency,
-  loan_value: currency
+export type Loan = Omit<
+  LoanStringlyTyped,
+  'available_amount' | 'annualised_return' | 'term_remaining' | 'ltv' | 'loan_value'
+> & {
+  available_amount: currency;
+  annualised_return: currency;
   term_remaining: Date;
+  ltv: BigInt;
+  loan_value: currency;
 };
-
