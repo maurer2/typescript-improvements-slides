a5129b82091144f2ff4082c8fc52694d07eb5c42
Martin <mr.mmaurer+work@gmail.com>
2025-04-28 20:39:03 +0200

    Added type to union type example

---
+++ code/examples/clever stuff/type-to-union.ts
@@ -0,0 +1,34 @@
+// https://matiashernandez.dev/blog/post/typescript-create-a-union-from-a-type
+// https://www.totaltypescript.com/tips/derive-a-union-type-from-an-object
+
+const reducerActionTypes = ['increment', 'decrement', 'reset'] as const;
+type ReducerActionTypes = typeof reducerActionTypes[number];
+
+type ReducerActionsNested = {
+  readonly [K in ReducerActionTypes]: {
+    type: K;
+    payload: K extends 'reset' ? void : number;
+  };
+};
+
+type ReducerActionsFlat = {
+  readonly [K in ReducerActionTypes]: {
+    type: K;
+    payload: K extends 'reset' ? void : number;
+  };
+}[ReducerActionTypes]; // magic
+
+const numberReducer = (state: number, action: ReducerActionsFlat) => {
+  switch (action.type) {
+    case 'increment':
+      return state + action.payload;
+    case 'decrement':
+      return state - action.payload;
+    case 'reset':
+      return 0;
+    default:
+      return action satisfies never;
+  }
+};
+
+numberReducer(0, { type: 'increment', payload: 1 });
