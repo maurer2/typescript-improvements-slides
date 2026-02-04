4d3950ac3bc3c5754b94fad5ee0bad3fa97fdda4
Martin <mr.mmaurer+work@gmail.com>
2026-02-04 22:49:52 +0100

    added some type tests

---
+++ code/examples/testing/sum.ts
@@ -0,0 +1,36 @@
+export function sumOverload(a: number, b: number): number;
+export function sumOverload(a: string, b: string): string;
+export function sumOverload(a: bigint, b: bigint): bigint;
+// eslint-disable-next-line max-len
+export function sumOverload(a: number | string | bigint, b: number | string | bigint): number | string | bigint {
+  if (typeof a === 'bigint' && typeof b === 'bigint') {
+    return BigInt(a + b);
+  }
+
+  if (typeof a === 'string' && typeof b === 'string') {
+    // return type not enforced here
+    return `${a}${b}`;
+  }
+
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a + b;
+  }
+
+  throw new Error('Invalid combination of argument types');
+}
+
+export function sumGeneric<T>(a: T, b: T): T {
+  if (typeof a === 'bigint' && typeof b === 'bigint') {
+    return BigInt(a + b) as T;
+  }
+
+  if (typeof a === 'string' && typeof b === 'string') {
+    return `${a}${b}` as T;
+  }
+
+  if (typeof a === 'number' && typeof b === 'number') {
+    return (a + b) as T;
+  }
+
+  throw new Error('Invalid combination of argument types');
+}
---
+++ code/examples/testing/testing-types-with-vitest.spec-d.ts
@@ -0,0 +1,49 @@
+import { describe, it, expectTypeOf } from 'vitest';
+
+import { sumOverload, sumGeneric } from './sum';
+
+describe('sumOverload', () => {
+  it('returns bigint number for bigint parameters', () => {
+    const sum = sumOverload(BigInt(Number.MAX_SAFE_INTEGER), BigInt(Number.MAX_SAFE_INTEGER));
+    expectTypeOf(sum).toBeBigInt();
+  });
+  it('returns concatenated string for string parameters', () => {
+    const sum = sumOverload('10', '20');
+    expectTypeOf(sum).toBeString();
+  });
+  it('returns number for number parameters', () => {
+    const sum = sumOverload(10, 20);
+    expectTypeOf(sum).toBeNumber();
+  });
+  it('returns never/Error for invalid combination of parameters', () => {
+    // @ts-expect-error invalid combination of parameter types
+    const sum = sumOverload('10', 20);
+    expectTypeOf(sum).toBeNever();
+  });
+});
+
+describe('sumGeneric', () => {
+  it('returns bigint number for bigint parameters', () => {
+    const sum = sumGeneric(BigInt(Number.MAX_SAFE_INTEGER), BigInt(Number.MAX_SAFE_INTEGER));
+    expectTypeOf(sum).toBeBigInt();
+  });
+  it('returns concatenated string for string parameters', () => {
+    const sum = sumGeneric('10', '20');
+    expectTypeOf(sum).toBeString();
+  });
+  it('returns number for number parameters', () => {
+    const sum = sumGeneric(10, 20);
+    expectTypeOf(sum).toBeNumber();
+  });
+  it('returns never/Error for invalid combination of parameters', () => {
+    // @ts-expect-error invalid combination of parameter types
+    const sum = sumGeneric(10, '10');
+    // should be never, but is 10
+    expectTypeOf(sum).not.toBeNever();
+
+    // @ts-expect-error invalid combination of parameter types
+    const sum2 = sumGeneric('10', 10);
+    // should be never, but is '10'
+    expectTypeOf(sum2).not.toBeNever();
+  });
+});
